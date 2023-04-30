fe15dce1886e78f5c5c874926994e29452227ec3
Nakayama Daiju <42.daiju@gmail.com>
2023-04-30 18:20:00 +0900

    wip

--- README.md
+++ README.md
@@ -14,3 +14,19 @@ https://twitter.com/Science_Release/status/1440659660961247242
 * 一番最初に10個全てが現れるのは 2⁶⁸=295147905179352825856
 
 0乗から199乗までの「もっとも出現しない数値の個数」のデータが [0-199.txt](./0-199.txt)。
+
+## ベンチマーク
+
+内部を十進数で表現。
+
+```
+$ time ./target/release/pow2-decimal > /dev/null
+      239.33 real       238.42 user         0.60 sys
+```
+
+内部を百進数で表現、変わらない。
+
+```
+$ /usr/bin/time ./target/release/pow2-decimal > /dev/null
+      225.09 real       223.88 user         0.61 sys
+```
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ fn pow2(n: &mut N) {
     let mut c = 0;
     for x in n.iter_mut() {
         *x = *x * 2 + c;
-        if *x >= 10 {
-            *x -= 10;
+        if *x >= 100 {
+            *x -= 100;
             c = 1;
         } else {
             c = 0;
@@ -24,25 +24,28 @@ fn test_pow2() {
     for _ in 0..10 {
         pow2(&mut n);
     }
-    assert_eq!(n, vec![8, 4, 0, 2]);
+    assert_eq!(n, vec![48, 20]);
 }
 
 fn count_digit(n: &N) -> [u32; 10] {
     let mut count = [0; 10];
     for &x in n.iter() {
-        count[x as usize] += 1;
+        if x >= 10 {
+            count[(x % 10) as usize] += 1;
+            count[(x / 10) as usize] += 1;
+        } else {
+            count[x as usize] += 1;
+        }
     }
     count
 }
 
 fn main() {
     let mut n = vec![1];
-    let mut e = 0u64;
-    loop {
+    for e in 0..1_000_000 {
         let digit = count_digit(&n);
         let c = *digit.iter().min().unwrap();
         println!("{} {}", e, c);
         pow2(&mut n);
-        e += 1;
     }
 }
